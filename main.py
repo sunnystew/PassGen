995886874c3b26e8944eedced2003adaa1ee1aa3
sunnystew <sunnystew22@gmail.com>
2025-12-05 19:47:39 +0330

    Updated to version 1.1
    New features:
    Added a way to generate more passwords without needing to re-run the program

--- main.py
+++ main.py
@@ -7,13 +7,28 @@ ______             _____
 | |_/ /_ _ ___ ___| |  \/ ___ _ __  
 |  __/ _` / __/ __| | __ / _ \ '_ \ 
 | | | (_| \__ \__ \ |_\ \  __/ | | |
-\_|  \__,_|___/___/\____/\___|_| |_| v1.0
+\_|  \__,_|___/___/\____/\___|_| |_| v1.1
                                     
 Made by sunnystew on GitHub
 
 """)
-password = printable[randint(0,93)]
+
+password = "0"
+
+
 for i in range(7):
-	password += printable[randint(0,93)]
+    password += printable[randint(0,93)]
+
 print("Your random password is:")
 print(password)
+
+while True:
+    again = input("\nAgain (y/n)? ")
+    if again.lower() == "y":
+        password = "0"
+        for i in range(7):    
+                password += printable[randint(0,93)] 
+        print("\nYour random password is:")
+        print(password)
+    else:
+        exit()
